2c773ad53451a9c91bcf9b9f8c9b463cb3f5ced5
Jureerat Saenchom <jureerat.dev@gmail.com>
2025-05-22 08:36:17 +0700

    Update config.py

--- Back/config.py
+++ Back/config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 def initialize_firebase():
-    cred = credentials.Certificate("/Users/kaew/Desktop/ngebakadfl/test-firebase-fee55-firebase-adminsdk-fbsvc-4a4a20bc05.json")
+    cred = credentials.Certificate("./test-firebase-fee55-firebase-adminsdk-fbsvc-4a4a20bc05.json")
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://test-firebase-fee55-default-rtdb.asia-southeast1.firebasedatabase.app/"
     })
