e4788916020ae40e1467af072f835cc35643ea94
Jureerat Saenchom <jureerat.dev@gmail.com>
2026-01-08 13:11:15 +0700

    Update config.py

--- Back/config.py
+++ Back/config.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
 
 def initialize_firebase():
-    cred = credentials.Certificate("./test-firebase-fee55-firebase-adminsdk-fbsvc-4a4a20bc05.json")
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    cred_path = os.path.join(base_path, "painproject-419f0-firebase-adminsdk-fbsvc-15c1537a76.json")
+    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred, {
-        "databaseURL": "https://test-firebase-fee55-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        "databaseURL": "https://painproject-419f0-default-rtdb.asia-southeast1.firebasedatabase.app/"
     })
